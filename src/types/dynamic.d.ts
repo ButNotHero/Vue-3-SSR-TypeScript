835b4594821862af1d5cbc7366e04967b0da3ddb
Danil Kondrachuk <butnothero@gmail.com>
2021-09-28 23:22:55 +0700

    chore: Removed array dynamic type

--- src/types/dynamic.d.ts
+++ src/types/dynamic.d.ts
@@ -1,17 +1,3 @@
 export interface dynamicObject {
   [name: string]: any;
 }
-
-/**
- * How to use:
- * const array = new DynamicArray<string>();
- * array.add('foo');
- * array.add(12); // will fail
- */
-class DynamicArray<T> {
-  add(value: T): Array<T> {
-    const collection = new Array<T>();
-    collection.push(value);
-    return collection;
-  }
-}
